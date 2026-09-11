6d215d1b8d17b2ef0650aead112b51bb87dd412c
dania-it <hpleen276@gmail.com>
2026-09-11 16:23:17 +0300

    Fix asset and script relative paths for GitHub Pages

--- JS/script.js
+++ JS/script.js
@@ -183,7 +183,7 @@ function renderSkeleton() {
 
 async function fetchBukhariJSON() {
     try {
-        const response = await fetch('/data/bukhari.json');
+        const response = await fetch('./data/bukhari.json');
         if (!response.ok) {
             throw new Error(`HTTP Error: ${response.status}`);
         }
